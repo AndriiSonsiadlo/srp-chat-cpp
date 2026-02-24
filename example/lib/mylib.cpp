7cf4c54df78653496098550cc285456218d61ef1
Andrii Sonsiadlo <andrii.sonsiadlo@gmail.com>
2026-02-24 11:08:46 +0100

    refactor: remove "example" project source files and build config

--- example/lib/mylib.cpp
+++
@@ -1,9 +0,0 @@
-#include "include/mylib.h"
-
-std::string MyLib::greet(const std::string& name) {
-    return "Hello, " + name + "!";
-}
-
-int MyLib::add(const int a, const int b) {
-    return a + b;
-}
--- example/src/app.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "mylib.h"
-#include "app.h"
-
-void runAppExample()
-{
-    std::cout << "Running app example..." << std::endl;
-    std::cout << MyLib::greet("C++ Developer") << std::endl;
-}
